0b3e601f4125af17dd60a0de9d9ee22ca327cb07
huangzhitao-pts <zhitao.huang@points.org>
2020-12-18 13:09:26 +0800

    deployment

--- app/view.py
+++ app/view.py
@@ -7,14 +7,11 @@ import yaml
 import paramiko
 
 
-NEW_IMAGE_TAG = set()
-
-
 auto_update = Blueprint("auto_update", __name__)
 
 
-@auto_update.route("/version/up")
-def version_up():
+@auto_update.route("/version/<opt>")
+def version_opt(opt):
     client = docker.from_env()
 
     # 是否有更新
@@ -30,16 +27,20 @@ def version_up():
     for _, svc in yaml_dict["services"].items():
         # 当前运行镜像的short_id
         print(svc["image"])
-        current_image_id = client.images.get(svc["image"]).short_id
-        print(current_image_id)
+        # current_image_id = client.images.get(svc["image"]).short_id
+        current_container = client.containers.get(svc["container_name"])
+        print(current_container)
 
-        svc["image"] = svc["image"].replace("stable", "beta")
+        # 更改待运行镜像版本
+        version_replace = ["stable", "beta"] if opt == "up" else ["beta", "stable"]
+        svc["image"] = svc["image"].replace(*version_replace)
+        
         # 更新镜像
         print(f"Pull image {svc['image']}")
         new_image = client.images.pull(svc["image"])
         print(new_image.short_id)
         # 有需要更新的镜像
-        if current_image_id != new_image.short_id and not FLAG:
+        if current_container.image.short_id != new_image.short_id and not FLAG:
             FLAG = True
     print(FLAG)
     if FLAG:
@@ -52,12 +53,9 @@ def version_up():
         ssh = paramiko.SSHClient()
         ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
         ssh.connect(hostname=app.config.get("LOCAL_HOST"), port=22, username='root')
-        print(f'docker-compose --project-directory {local_run_path} -f {os.path.join(local_run_path, yaml_path[1:])} up -d')
-        stdin, stdout, stderr = ssh.exec_command(
-            f'docker-compose --project-directory {local_run_path} -f {os.path.join(local_run_path, yaml_path[1:])} up -d')
-        # print(stdin.read().decode())
-        print(stdout.read().decode())
-        # print(stderr.read().decode())
+        print(f'cd {local_run_path} && docker-compose -f {"." + yaml_path} up -d')
+        ssh.exec_command(
+            f'cd {local_run_path} && docker-compose -f {"." + yaml_path} up -d')
         ssh.close()
 
     return "ok"
